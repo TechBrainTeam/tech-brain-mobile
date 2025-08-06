fe9d49259a155ee5e2fc8cd0016f30f93bb42d80
Serhat Simsek <srhtsimsekk@gmail.com>
2025-08-06 23:04:27 +0300

    feat: add Constants file for app-wide configuration

---
+++ FobiniYen/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  FobiniYen
+//
+//  Created by Serhat  Şimşek  on 2.08.2025.
+//
+
+struct Constants {
+    struct UserDefaults {
+        static let isFirstLaunch = "İsFirstLaunch?"
+    }
+    
+    struct URLPaths {
+        static let BASE_URL = "https://tech-brain-api.onrender.com"
+    }
+}
